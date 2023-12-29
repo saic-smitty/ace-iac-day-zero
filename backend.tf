08099fa606dbe8bb1e1c09a266727ccec534989b
saic-smitty <152712078+saic-smitty@users.noreply.github.com>
2023-12-29 03:17:05 -0600

    Update backend.tf
    updated terraform.io organization

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "jsmith3205"
+    organization = "ACE-IAC"
     workspaces {
       name = "ace-iac-day-zero"
     }
